f1b48ea9f003c1c1f2c1bd4bc5026bdf9586fd0e
Ruben Zilibowitz <rzilibowitz@deputy.com>
2017-04-04 19:10:05 +1000

    primes two layers

--- DeepLearning/Experiments.hs
+++ DeepLearning/Experiments.hs
@@ -2,8 +2,9 @@ module Main where
 
 import System.Random
 import Data.List.Split
-import Data.Dif
 import Data.Numbers.Primes
+import Data.Matrix
+import Data.Dif
 
 differentiable :: Num a => [a] -> [Dif Int a]
 differentiable zs = zipWith (\i z -> dId i z) [1..] zs
@@ -12,15 +13,55 @@ gradient :: Num a => ([Dif Int a] -> Dif Int b) -> [a] -> [Dif Int b]
 gradient f params = [deriv y i | i <- [1 .. (length params)]]
  where y = f (differentiable params)
 
+neuralNetInputs :: Int
+neuralNetInputs = 2
+
 neuralNetWidth :: Int
-neuralNetWidth = 5
+neuralNetWidth = 3
+
+neuralNetDepth :: Int
+neuralNetDepth = 2
+
+numberOfParameters :: Int
+numberOfParameters = (neuralNetDepth - 1)*neuralNetWidth^2 + neuralNetInputs*neuralNetWidth + neuralNetWidth + neuralNetDepth*neuralNetWidth + 1
+
+logistic :: Floating a => a -> a
+logistic = recip (1+(exp negate))
 
 activation :: Floating a => a -> a
-activation = recip (1+(exp negate))
+activation = logistic
 
+neuralNet :: Floating t => [t] -> t -> t -> t
+neuralNet params u v = (((transpose w2) * (fmap activation (w1 * (fmap activation ((w0 * x) + c)) + b))) + d) ! (1,1)
+ where
+  -- construct input vector
+  x = fromList 2 1 [u,v]
+  
+  -- extract the various components
+  [w0_param,c_param,w1_param,b_param,w2_param,d_param] = splitPlaces [2*neuralNetWidth,neuralNetWidth,neuralNetWidth^2,neuralNetWidth,neuralNetWidth,1] params
+  
+  -- construct matrices and vectors
+  w0 = fromList neuralNetWidth 2 w0_param
+  c = fromList neuralNetWidth 1 c_param
+  w1 = fromList neuralNetWidth neuralNetWidth w1_param
+  b = fromList neuralNetWidth 1 b_param
+  w2 = fromList neuralNetWidth 1 w2_param
+  d = fromList 1 1 d_param
+
+{-
+neuralNet :: Floating a => [a] -> a -> a -> a
+neuralNet params u v = sum (zipWith (*) (zipWith3 (\x y z -> x + y + z) (zipWith (*) w00 (repeat u)) (zipWith (*) w01 (repeat v)) c) w1) + b
+ where
+  [w00,w01,c,w1,[b]] = splitPlaces [neuralNetWidth,neuralNetWidth,neuralNetWidth,neuralNetWidth,1] params
+-}
+
+{-
 neuralNet :: Floating a => [a] -> a -> a
-neuralNet params z = sum (zipWith (*) w1 (map activation (zipWith (+) (map (z *) w0) c))) + b
- where [w0,c,w1,[b]] = splitPlaces [neuralNetWidth,neuralNetWidth,neuralNetWidth,1] params
+neuralNet params x = sum (zipWith (*) w2 (applyLayer w1 c1 (applyLayer w0 c0 (repeat x)))) + c2
+ where
+  [w0,c0,w1,c1,w2,[c2]] = splitPlaces [neuralNetWidth,neuralNetWidth,neuralNetWidth,neuralNetWidth,neuralNetWidth,1] params
+  applyLayer w c xs = map activation (zipWith (+) (zipWith (*) w xs) c)
+-}
 
 myFloor :: (Show b, Read a) => b -> a
 myFloor x = read (head (splitOn "." (show x)))
@@ -28,12 +69,12 @@ myFloor x = read (head (splitOn "." (show x)))
 myRound :: (Show b, Read a, Fractional b) => b -> a
 myRound x = myFloor (x+0.5)
 
-target :: (Floating a, Read a, Show a) => a -> a
-target x = if isPrime (myRound x) then 1 else 0
+target :: (Floating a, Read a, Show a) => a -> a -> a
+target u v = let x = 3*u + v in if isPrime (myRound x) then 1 else 0 --  3*u + v - 2*u*v
 
 neuralNet_Error :: (Floating a, Read a, Show a) => [a] -> a
-neuralNet_Error params = sum [let z = fromIntegral z_ in ((neuralNet params z) - (target z))^2 / 2 | z_ <- inputSpace]
- where inputSpace = [1..11]
+neuralNet_Error params = sum [let (u,v) = (fromIntegral a,fromIntegral b) in ((neuralNet params u v) - (target u v))^2 / 2 | (a,b) <- inputSpace]
+ where inputSpace = [(u,v) | u <- [0..2], v <- [0..2]]
 
 neuralNet_Error_Gradient :: (Floating a, Read a, Show a) => [a] -> [a]
 neuralNet_Error_Gradient params = map dVal (gradient neuralNet_Error params)
@@ -80,13 +121,13 @@ neuralNet_gradient_descent startingParams = iterate neuralNet_update startingPar
 
 main = do
   -- generate some random numbers as our initial parameters
-  let rs = map ((2*) - 1) (randoms (mkStdGen 3) :: [Double])
+  let rs = [-5.997964617436484,6.8366753807468905,1.9946181529853286,-2.0013185819246995,-3.072544454848253,3.1034310008887287,-5.979828742108006,-3.1902084692473425,-1.4136501174627154,1.2927066753777938,-0.7099211453394163,-2.0648259100507986,4.545574047629832,-1.210595301923914,-4.532966851592963,0.9353161955234404,-0.9619315229579541,-2.0069557079385962,-0.23942932061733338,0.2858828985321634,-0.17420719349960065,1.8978890423090384,5.705283980931233,1.7484994317561444,-3.2301006289511487]
   
   -- run gradient descent
-  let ps = neuralNet_gradient_descent (take (4*neuralNetWidth + 1) rs)
+  let ps = neuralNet_gradient_descent (take numberOfParameters rs)
   
   -- obtain one of the iterations of gradient descent
-  let p = (ps !! 10000)
+  let p = (ps !! 1000)
   
   -- print the new parameters
   print p
