d237a9f49151aafa316a1c97086323357026c143
Ruben Zilibowitz <rzilibowitz@deputy.com>
2017-07-16 23:01:49 +1000

    bug fixes

--- DeepLearning/Tracking/Tracking.hs
+++ DeepLearning/Tracking/Tracking.hs
@@ -9,12 +9,13 @@ import BoxTrack
 
 import Data.Colour.RGBSpace
 import Data.Colour.RGBSpace.HSL
-import Codec.Picture( PixelRGBA8( .. ), writePng )
+
+import Codec.Picture
 import Graphics.Text.TrueType( loadFontFile )
 import Graphics.Rasterific
 import Graphics.Rasterific.Texture
 
---import Debug.Trace
+import Debug.Trace
 
 testFile = "/Users/ruben/Documents/thirdparty/darknet/airplane-formation-boxes.txt"
 imagesDirectory = "/Users/ruben/Documents/thirdparty/darknet/airplane-formation/"
@@ -28,10 +29,10 @@ drawBoxes n imageFilename boxes = do
   let white = PixelRGBA8 255 255 255 255
       img = renderDrawing (imageWidth currentImage) (imageHeight currentImage) white $ do
         drawImageAtSize currentImage 0 (V2 0 0) (fromIntegral (imageWidth currentImage)) (fromIntegral (imageHeight currentImage))
-        sequence_ [let (RGB r g b) = fmap (floor . (* 255)) $ hsl (fromIntegral (label*5)) 0.54 0.5
+        sequence_ [traceShow (label,(left,right,up,down),kind) $ let (RGB r g b) = fmap (floor . (* 255)) $ hsl (fromIntegral (label*5)) 0.54 0.5
                    in withTexture (uniformTexture (PixelRGBA8 r g b 255)) $ do
                        stroke lineWidth JoinRound (CapRound, CapRound) $ rectangle (V2 left up) (right-left) (down-up)
-                       printTextAt font (PointSize 12) (V2 (left+12) (up+24)) (show label) | Box left right up down kind (Just label) <- boxes]
+                       printTextAt font (PointSize 12) (V2 (left+12) (up+24)) (show label ++ " " ++ kind) | Box left right up down (Just kind) (Just label) <- boxes]
   
   writePng ("output/" ++ show n ++ ".png") img
 
