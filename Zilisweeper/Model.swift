bff6ef61c371ff3e013b16b93ecf05c989087fee
Ruben Zilibowitz <rzilibowitz@deputy.com>
2017-11-07 10:15:40 +1100

    refactor

--- Zilisweeper/Model.swift
+++ Zilisweeper/Model.swift
@@ -166,4 +166,27 @@ class Model {
         
         return Array(UnsafeBufferPointer(start: result, count: N))
     }
+    
+    func boardSuggest(board:Board) -> (free:Bool, row: Int, col: Int)? {
+        var x:[Float] = Array(repeating: -1, count: 1440)
+        
+        for (i,a) in board.adjacency.enumerated() {
+            var oneHotVec:[Float] = Array(repeating: 0, count: 10)
+            oneHotVec[a+1] = 1
+            for j in 0 ..< 10 {
+                x[j + i*10] = oneHotVec[j]
+            }
+        }
+        
+        guard let y = run(input: x), let max = y.max() else { return nil }
+        guard let i = y.index(of: max) else { return nil }
+        print(i)
+        if i < 144 {
+            return (free: true, row: i / 12, col: i % 12)
+        }
+        else {
+            let j = i - 144
+            return (free: false, row: j / 12, col: j % 12)
+        }
+    }
 }
--- Zilisweeper/ViewController.swift
+++ Zilisweeper/ViewController.swift
@@ -27,26 +27,8 @@ class ViewController: UIViewController {
     }
     
     @IBAction func tappedButton(_ sender: UIButton) {
-        var x:[Float] = Array(repeating: -1, count: 1440)
-        
-        for (i,a) in board.adjacency.enumerated() {
-            var oneHotVec:[Float] = Array(repeating: 0, count: 10)
-            oneHotVec[a+1] = 1
-            for j in 0 ..< 10 {
-                x[j + i*10] = oneHotVec[j]
-            }
-        }
-        
-        guard let y = Model.shared.run(input: x), let max = y.max() else { return }
-        guard let i = y.index(of: max) else { return }
-        print(i)
-        if i < 144 {
-            print("free at \((i / 12, i % 12))")
-        }
-        else {
-            let j = i - 144
-            print("mine at \((j / 12, j % 12))")
-        }
+        guard let result = Model.shared.boardSuggest(board: board) else { return }
+        print(result)
     }
     
     @IBAction func tappedBoardView(_ sender: UITapGestureRecognizer) {
