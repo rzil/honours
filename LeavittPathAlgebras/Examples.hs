6214f09c4f3f5e51f363803332f28840378ed1fc
Ruben Zilibowitz <ruben.zilibowitz@gmail.com>
2018-10-18 13:08:11 +0800

    only wpla

--- LeavittPathAlgebras/Examples.hs
+++ LeavittPathAlgebras/Examples.hs
@@ -1,7 +1,6 @@
 module Examples where
 
-import WeightedLPA hiding (atom, vertex, edge, ghostEdge)
-import LPA (atom, vertex, edge, ghostEdge)
+import qualified WeightedLPA as WLPA
 import Graph
 import qualified Data.Map as M
 import qualified Data.Set as S
@@ -12,8 +11,12 @@ weighted_example = WeightedGraph (buildGraphFromEdges [("e",("v","u")),("f",("v"
 unweighted_equivalent_example :: Graph String String
 unweighted_equivalent_example = Graph (S.fromList ["u1","u2","u3","v"]) (M.fromList [("j",("u3","u3")),("i",("u3","u1")),("h",("u3","u2")),("g",("u2","u1")),("e1",("v","u1")),("e2",("v","u2")),("e3",("v","u3")),("f",("v","u2"))])
 
-u = (atom (vertex "u1")) + (atom (vertex "u2")) + (atom (vertex "u3"))
-v = atom (vertex "v")
-e1 = (atom (edge "e1")) + (atom (edge "e2")) + (atom (edge "e3"))
-f1 = atom (edge "f")
-f2 = (atom (edge "f")) * (atom (edge "g")) + (atom (edge "e1")) * (atom (ghostEdge "i")) + (atom (edge "e2")) * (atom (ghostEdge "h")) + (atom (edge "e3")) * (atom (ghostEdge "j"))
+vertex = WLPA.atom . WLPA.vertex
+edge = WLPA.atom . (flip WLPA.edge 1)
+ghostEdge = WLPA.atom . (flip WLPA.ghostEdge 1)
+
+u = (vertex "u1") + (vertex "u2") + (vertex "u3")
+v = vertex "v"
+e1 = (edge "e1") + (edge "e2") + (edge "e3")
+f1 = edge "f"
+f2 = (edge "f") * (edge "g") + (edge "e1") * (ghostEdge "i") + (edge "e2") * (ghostEdge "h") + (edge "e3") * (ghostEdge "j")
--- LeavittPathAlgebras/Main.hs
+++ LeavittPathAlgebras/Main.hs
@@ -1,7 +1,6 @@
 module Main where
 
 import Graph
-import qualified LPA as LPA
 import qualified WeightedLPA as WLPA
 import qualified Data.Set as S
 import qualified Data.Map as M
@@ -13,19 +12,18 @@ import Control.Monad.Omega (runOmega, each)
 import Examples
 
 isoMapEdge (WLPA.NormalFormEdge "e" False 1) = e1
-isoMapEdge (WLPA.NormalFormEdge "e" True 1) = LPA.adjoint e1
+isoMapEdge (WLPA.NormalFormEdge "e" True 1) = WLPA.adjoint e1
 isoMapEdge (WLPA.NormalFormEdge "f" False 1) = f1
-isoMapEdge (WLPA.NormalFormEdge "f" True 1) = LPA.adjoint f1
+isoMapEdge (WLPA.NormalFormEdge "f" True 1) = WLPA.adjoint f1
 isoMapEdge (WLPA.NormalFormEdge "f" False 2) = f2
-isoMapEdge (WLPA.NormalFormEdge "f" True 2) = LPA.adjoint f2
+isoMapEdge (WLPA.NormalFormEdge "f" True 2) = WLPA.adjoint f2
 
 isoMapVertex "v" = v
 isoMapVertex "u" = u
 
-isoMapPath :: WLPA.NormalFormAtom String String Integer -> LPA.NormalForm String String Integer
-isoMapPath (WLPA.NormalFormAtom 1 vertex []) = LPA.convertTermToBasis unweighted_equivalent_example (isoMapVertex vertex)
-isoMapPath (WLPA.NormalFormAtom 1 vertex path) = LPA.convertTermToBasis unweighted_equivalent_example ((isoMapVertex vertex) * (foldl1 (*) (map isoMapEdge path)))
-isoMapPath (WLPA.NormalFormAtom c vertex path) = map (\(LPA.NormalFormAtom k v es fs) -> LPA.NormalFormAtom (c*k) v es fs) (isoMapPath (WLPA.NormalFormAtom 1 vertex path))
+isoMapPath (WLPA.NormalFormAtom 1 vertex []) = WLPA.convertToBasisForm (convertGraphToWeighted unweighted_equivalent_example) (isoMapVertex vertex)
+isoMapPath (WLPA.NormalFormAtom 1 vertex path) = WLPA.convertToBasisForm (convertGraphToWeighted unweighted_equivalent_example) ((isoMapVertex vertex) * (foldl1 (*) (map isoMapEdge path)))
+isoMapPath (WLPA.NormalFormAtom c vertex path) = map (\(WLPA.NormalFormAtom k v es) -> WLPA.NormalFormAtom (c*k) v es) (isoMapPath (WLPA.NormalFormAtom 1 vertex path))
 
 verify
   :: WeightedGraph String String
@@ -38,8 +36,8 @@ verify weightedGraph unweightedGraph x y = sort fxy == sort fxfy
   xy = WLPA.convertToBasisForm weightedGraph ((WLPA.convertTerm x) * (WLPA.convertTerm y))
   fx = isoMapPath x
   fy = isoMapPath y
-  fxy = LPA.collectNormalFormAtoms $ concatMap isoMapPath xy
-  fxfy = LPA.convertTermToBasis unweightedGraph $ (sum $ map LPA.convertTerm $ isoMapPath x) * (sum $ map LPA.convertTerm $ isoMapPath y)
+  fxy = WLPA.collectNormalFormAtoms $ concatMap isoMapPath xy
+  fxfy = WLPA.convertToBasisForm (convertGraphToWeighted unweightedGraph) $ (sum $ map WLPA.convertTerm $ isoMapPath x) * (sum $ map WLPA.convertTerm $ isoMapPath y)
 
 findFails weightedGraph unweightedGraph = filter (not . fst) $ runOmega $ liftM2 (\(i,x) (j,y) -> (verify weightedGraph unweightedGraph x y,(i,j))) (each bs) (each bs)
  where bs = zip [0..] (WLPA.basis weightedGraph)
@@ -60,8 +58,8 @@ test weightedGraph unweightedGraph x y = do
   xy = WLPA.convertToBasisForm weightedGraph ((WLPA.convertTerm x) * (WLPA.convertTerm y))
   fx = isoMapPath x
   fy = isoMapPath y
-  fxy = LPA.collectNormalFormAtoms $ concatMap isoMapPath xy
-  fxfy = LPA.convertTermToBasis unweightedGraph $ (sum $ map LPA.convertTerm $ isoMapPath x) * (sum $ map LPA.convertTerm $ isoMapPath y)
+  fxy = WLPA.collectNormalFormAtoms $ concatMap isoMapPath xy
+  fxfy = WLPA.convertToBasisForm (convertGraphToWeighted unweightedGraph) $ (sum $ map WLPA.convertTerm $ isoMapPath x) * (sum $ map WLPA.convertTerm $ isoMapPath y)
 
 main = test weighted_example unweighted_equivalent_example x y
    
