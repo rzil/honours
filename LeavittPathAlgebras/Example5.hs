c2010969b60f02cce43ca712e19fc62dfaf1823f
Ruben Zilibowitz <ruben.zilibowitz@gmail.com>
2019-06-04 20:12:57 +0700

    explanation

--- LeavittPathAlgebras/Example5.hs
+++ LeavittPathAlgebras/Example5.hs
@@ -6,10 +6,10 @@ import qualified Data.Map as M
 import qualified Data.Set as S
 
 weighted_example :: WeightedGraph String String
-weighted_example = WeightedGraph (buildGraphFromEdges [("e",("v","u")), ("f",("u","w"))]) (M.fromList [("e",2),("f",1)])
+weighted_example = WeightedGraph (buildGraphFromEdges [("g",("x","v")),("e",("v","u")), ("f",("u","w"))]) (M.fromList [("e",2),("f",1),("g",1)])
 
 unweighted_equivalent_example :: Graph String String
-unweighted_equivalent_example = (buildGraphFromEdges [("e1",("u","v")), ("e2",("u","v")), ("f",("w","u"))])
+unweighted_equivalent_example = (buildGraphFromEdges [("e1",("u","v")), ("e2",("u","v")), ("f",("w","u")),("g",("x","v"))])
 
 atom c = WLPA.Atom c
 vertex = atom 1 . WLPA.vertex
@@ -21,20 +21,24 @@ ghostEdge2 = atom 1 . (flip WLPA.ghostEdge 2)
 
 s = WLPA.adjoint
 
+x = vertex "x"
 u = vertex "u"
 v = vertex "v"
 w = vertex "w"
 e1 = edge "e1"
 e2 = edge "e2"
 f = edge "f"
+g = edge "g"
 
 testHomomorphism = WLPA.wLPA_relations_present phi weighted_example (convertGraphToWeighted unweighted_equivalent_example)
  where
+  phi (WLPA.AVertex "x") = x
   phi (WLPA.AVertex "u") = u
   phi (WLPA.AVertex "v") = v
   phi (WLPA.AVertex "w") = w
   phi (WLPA.AEdge "e" 1) = s e1
   phi (WLPA.AEdge "e" 2) = s e2
   phi (WLPA.AEdge "f" 1) = s f
+  phi (WLPA.AEdge "g" 1) = g
   phi (WLPA.AGhostEdge e w) = WLPA.adjoint (phi (WLPA.AEdge e w))
   phi _ = WLPA.Zero
